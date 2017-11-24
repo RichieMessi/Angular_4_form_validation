750e1493673c6e090c348c0828d164e69de6980d
Richardson <Messirichards@gmail.com>
2017-11-24 01:29:28 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // title = 'app';
+
+  name = 'Richie'
+
+  day = new Date(1992, 3, 15)
+
+  arr = [3, 4, 5, 6, 9, 0]
+
+  foo(e){
+    console.warn('works')
+    e.preventDefault()
+  }
+
+  onSubmit = function(x){
+    console.warn(x)
+  }
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { MembersComponent } from './members/members.component';
+import { ProductComponent } from './product/product.component';
+import { SortPipe } from './app.sort'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    SortPipe,
+    AppComponent,
+    MembersComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
